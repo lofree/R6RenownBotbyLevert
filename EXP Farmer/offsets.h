83cedd3e899b535f943598f67100ff891285779b
lofree <61709892+lofree@users.noreply.github.com>
2020-03-02 20:34:50 +0100

    Add files via upload

---
+++ EXP Farmer/offsets.h
@@ -0,0 +1,8 @@
+#include <Windows.h>
+#include <cstdint>
+//01-21-2020
+namespace Offsets {
+	constexpr std::uintptr_t GameManager = 0x5285AB8;
+	constexpr std::uintptr_t RoundManager = 0x5275388;
+	constexpr std::uintptr_t GameProfile = 0x5272D38;
+}
